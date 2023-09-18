2a567b1a252599e5f16acab311058171800e695f
Antonchik <tohichpetrov@gmail.com>
2023-09-18 18:52:20 +0300

    Refactor main.py part 2: decoding
    Like in part 1, the aim is to divide big function into logical parts, again NO NEW SPECIFICATION IMPLEMENTATION:
      - check_checksum()
      - check_signature()
      - read_starting_header_part()
      - unpack_and_save_file()

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from os import path, makedirs
 from struct import pack, unpack
+from typing import BinaryIO
 from zlib import crc32
 
 
 # struct directives: H for unsigned short,      2 bytes
 #                    I for unsigned int,        4 bytes
 #                    Q for unsigned long long,  8 bytes
+
+# Encoding part
 
 def create_starting_header_part(file_paths: list[str]) -> bytes:
     signature = b'PIN34MPR'
@@ -18,13 +21,13 @@ def create_starting_header_part(file_paths: list[str]) -> bytes:
 
 def create_file_header_part(file_path: str) -> bytes:
     relative_path = file_path.encode('utf-8')
-    rel_path_length = pack('H', len(relative_path))
+    relative_path_length = pack('H', len(relative_path))
     file_size = pack('Q', path.getsize(file_path))
 
     with open(file_path, 'rb') as f:
         file_content = f.read()
 
-    return rel_path_length + relative_path + file_size + file_content
+    return relative_path_length + relative_path + file_size + file_content
 
 
 def encode(file_paths: list[str], archive_path: str) -> None:
@@ -40,33 +43,62 @@ def encode(file_paths: list[str], archive_path: str) -> None:
         archive.write(checksum)
 
 
-# Code below is not yet refactored
+# Decoding part
+
+def check_checksum(archive_path: str) -> bool:
+    with open(archive_path, 'rb') as archive:
+        archive_content = archive.read()
+        archive_checksum = unpack('I', archive_content[-4:])[0]
+        new_checksum = crc32(archive_content[:-4])
+
+        if archive_checksum != new_checksum:
+            print("Invalid checksum")
+            return False
+
+        return True
+
+
+def check_signature(signature: bytes) -> bool:
+    if signature != b'PIN34MPR':
+        print("Invalid signature")
+        return False
+
+    return True
+
+
+def read_starting_header_part(archive: BinaryIO) -> tuple[bytes, int, int, int]:
+    signature = archive.read(8)
+    version = unpack('H', archive.read(2))[0]
+    algorithms_codes = unpack('I', archive.read(4))[0]
+    file_count = unpack('I', archive.read(4))[0]
+
+    return signature, version, algorithms_codes, file_count
+
+
+def unpack_and_save_file(archive: BinaryIO, output_folder: str) -> None:
+    relative_path_length = unpack('H', archive.read(2))[0]
+    relative_path = archive.read(relative_path_length).decode('utf-8')
+    file_size = unpack('Q', archive.read(8))[0]
+
+    output_path = path.join(output_folder, relative_path)
+    makedirs(path.dirname(output_path), exist_ok=True)
+
+    with open(output_path, 'wb') as f:
+        f.write(archive.read(file_size))
+
 
 def decode(archive_path: str, output_folder: str) -> None:
+    if not check_checksum(archive_path):
+        return
+
     with open(archive_path, 'rb') as archive:
-        signature = archive.read(8)
-        if signature != b'PIN34MPR':
-            print("Invalid signature")
+        signature, _, _, file_count = read_starting_header_part(archive)
+
+        if not check_signature(signature):
             return
 
-        # version = unpack('H', archive.read(2))[0]
-        archive.read(2)
-        # algorithms_codes = unpack('I', archive.read(4))[0]
-
-        file_count = unpack('I', archive.read(4))[0]
-        # header_checksum = unpack('I', archive.read(4))[0]
-        archive.read(4)
-
         for _ in range(file_count):
-            rel_path_length = unpack('H', archive.read(2))[0]
-            rel_path = archive.read(rel_path_length).decode('utf-8')
-            file_size = unpack('Q', archive.read(8))[0]
-
-            output_path = path.join(output_folder, rel_path)
-            makedirs(path.dirname(output_path), exist_ok=True)
-
-            with open(output_path, 'wb') as f:
-                f.write(archive.read(file_size))
+            unpack_and_save_file(archive, output_folder)
 
 
 if __name__ == '__main__':
