58b39e2b9c1ec15879f844cc667f5aaebbc57fa0
Antonchik <tohichpetrov@gmail.com>
2023-09-18 17:37:42 +0300

    Refactor main.py part 1 and slightly edit README.md
    main.py had to be refactored and split into independent functions. This commit changes encoding part, NO NEW SPECIFICATION IMPLEMENTATION:
      - create_starting_header_part()
      - create_file_header_part()

--- main.py
+++ main.py
@@ -1,60 +1,69 @@
-import os
-import struct
+from os import path, makedirs
+from struct import pack, unpack
+from zlib import crc32
 
 
-def create_header(file_path):
-    rel_path = file_path.encode('utf-8')
-    rel_path_length = struct.pack('H', len(rel_path))
-    file_size = struct.pack('Q', os.path.getsize(file_path))
-    return rel_path_length + rel_path + file_size
+# struct directives: H for unsigned short, 2 bytes
+#                    I for unsigned int, 4 bytes
+#                    Q for unsigned long long, 8 bytes
 
-
-def encode(file_paths, archive_path):
+def create_starting_header_part(file_paths: list[str]) -> bytes:
     signature = b'PIN34MPR'
-    version = struct.pack('H', 1)
-    algo_codes = struct.pack('I', 0)
-    file_count = struct.pack('I', len(file_paths))
+    version = pack('H', 1)
+    algorithms_codes = pack('I', 0)
+    file_count = pack('I', len(file_paths))
 
-    header_checksum = struct.pack('I', 0)  # Placeholder for checksum
-    header = signature + version + algo_codes + file_count + header_checksum
+    return signature + version + algorithms_codes + file_count
 
+
+def create_file_header_part(file_path: str) -> bytes:
+    relative_path = file_path.encode('utf-8')
+    rel_path_length = pack('H', len(relative_path))
+    file_size = pack('Q', path.getsize(file_path))
+
+    with open(file_path, 'rb') as f:
+        file_content = f.read()
+
+    return rel_path_length + relative_path + file_size + file_content
+
+
+def encode(file_paths: list[str], archive_path: str) -> None:
     with open(archive_path, 'wb') as archive:
-        archive.write(header)
+        starting_header_part = create_starting_header_part(file_paths)
+        archive.write(starting_header_part)
 
-        for path in file_paths:
-            file_header = create_header(path)
-            archive.write(file_header)
+        for file_path in file_paths:
+            file_header_part = create_file_header_part(file_path)
+            archive.write(file_header_part)
 
-            with open(path, 'rb') as f:
-                archive.write(f.read())
+        checksum = pack('I', crc32(archive.read()))
+        archive.write(checksum)
 
 
-def decode(archive_path, output_folder):
+# Code below is not yet refactored
+
+def decode(archive_path: str, output_folder: str) -> None:
     with open(archive_path, 'rb') as archive:
         signature = archive.read(8)
         if signature != b'PIN34MPR':
             print("Invalid signature")
             return
 
-        # version = struct.unpack('H', archive.read(2))[0]
+        # version = unpack('H', archive.read(2))[0]
         archive.read(2)
-        algo_codes = struct.unpack('I', archive.read(4))[0]
+        # algorithms_codes = unpack('I', archive.read(4))[0]
 
-        if algo_codes != 0:
-            print("Unsupported algorithm codes")
-            return
-
-        file_count = struct.unpack('I', archive.read(4))[0]
-        # header_checksum = struct.unpack('I', archive.read(4))[0]
+        file_count = unpack('I', archive.read(4))[0]
+        # header_checksum = unpack('I', archive.read(4))[0]
         archive.read(4)
 
         for _ in range(file_count):
-            rel_path_length = struct.unpack('H', archive.read(2))[0]
+            rel_path_length = unpack('H', archive.read(2))[0]
             rel_path = archive.read(rel_path_length).decode('utf-8')
-            file_size = struct.unpack('Q', archive.read(8))[0]
+            file_size = unpack('Q', archive.read(8))[0]
 
-            output_path = os.path.join(output_folder, rel_path)
-            os.makedirs(os.path.dirname(output_path), exist_ok=True)
+            output_path = path.join(output_folder, rel_path)
+            makedirs(path.dirname(output_path), exist_ok=True)
 
             with open(output_path, 'wb') as f:
                 f.write(archive.read(file_size))
