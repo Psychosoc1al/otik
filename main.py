97c0dc781cc2fe53d6e793162ecf068c51018323
Antonchik <tohichpetrov@gmail.com>
2023-09-18 20:58:06 +0300

    Correct main.py to follow new specification, minor changes
    Renamed check_checksum() to check_hashsum(), added SIGNATURE constant and placeholder functions to encode/decode extra fields

--- main.py
+++ main.py
@@ -3,20 +3,31 @@ from struct import pack, unpack
 from typing import BinaryIO
 from zlib import crc32
 
+# struct packing directives:
+#       B for unsigned char,       1 byte
+#       H for unsigned short,      2 bytes
+#       I for unsigned int,        4 bytes
 
-# struct directives: H for unsigned short,      2 bytes
-#                    I for unsigned int,        4 bytes
-#                    Q for unsigned long long,  8 bytes
+SIGNATURE = b'PIN34MPR'
+
 
 # Encoding part
 
 def create_starting_header_part(file_paths: list[str]) -> bytes:
-    signature = b'PIN34MPR'
-    version = pack('H', 1)
-    algorithms_codes = pack('I', 0)
-    file_count = pack('I', len(file_paths))
+    signature = SIGNATURE
+    version = pack('B', 1)
+    algorithms_codes = pack('B', 0)
+    extra_fields_data = create_extra_fields_header_part()
+    file_count = pack('H', len(file_paths))
 
-    return signature + version + algorithms_codes + file_count
+    return signature + version + algorithms_codes + extra_fields_data + file_count
+
+
+def create_extra_fields_header_part() -> bytes:
+    extra_fields_amount = pack('H', 0)
+    # some code in future
+
+    return extra_fields_amount
 
 
 def create_file_header_part(file_path: str) -> bytes:
@@ -45,7 +56,7 @@ def encode(file_paths: list[str], archive_path: str) -> None:
 
 # Decoding part
 
-def check_checksum(archive_path: str) -> bool:
+def check_hashsum(archive_path: str) -> bool:
     with open(archive_path, 'rb') as archive:
         archive_content = archive.read()
         archive_checksum = unpack('I', archive_content[-4:])[0]
@@ -59,26 +70,37 @@ def check_checksum(archive_path: str) -> bool:
 
 
 def check_signature(signature: bytes) -> bool:
-    if signature != b'PIN34MPR':
+    if signature != SIGNATURE:
         print("Invalid signature")
         return False
 
     return True
 
 
-def read_starting_header_part(archive: BinaryIO) -> tuple[bytes, int, int, int]:
+def read_starting_header_part(archive: BinaryIO) -> tuple[bytes, int, int, int, int]:
     signature = archive.read(8)
-    version = unpack('H', archive.read(2))[0]
-    algorithms_codes = unpack('I', archive.read(4))[0]
-    file_count = unpack('I', archive.read(4))[0]
+    version = unpack('B', archive.read(1))[0]
+    algorithms_codes = unpack('B', archive.read(1))[0]
+    extra_fields_amount = read_extra_fields_header_part(archive)  # placeholder to read 1 byte
+    file_count = unpack('H', archive.read(2))[0]
 
-    return signature, version, algorithms_codes, file_count
+    return signature, version, algorithms_codes, extra_fields_amount, file_count
+
+
+def read_extra_fields_header_part(archive: BinaryIO) -> int:
+    extra_fields_amount = unpack('B', archive.read(1))[0]
+
+    for _ in range(extra_fields_amount):
+        # some code in future
+        ...
+
+    return extra_fields_amount
 
 
 def unpack_and_save_file(archive: BinaryIO, output_folder: str) -> None:
     relative_path_length = unpack('H', archive.read(2))[0]
     relative_path = archive.read(relative_path_length).decode('utf-8')
-    file_size = unpack('Q', archive.read(8))[0]
+    file_size = unpack('Q', archive.read(4))[0]
 
     output_path = path.join(output_folder, relative_path)
     makedirs(path.dirname(output_path), exist_ok=True)
@@ -88,11 +110,11 @@ def unpack_and_save_file(archive: BinaryIO, output_folder: str) -> None:
 
 
 def decode(archive_path: str, output_folder: str) -> None:
-    if not check_checksum(archive_path):
+    if not check_hashsum(archive_path):
         return
 
     with open(archive_path, 'rb') as archive:
-        signature, _, _, file_count = read_starting_header_part(archive)
+        signature, _, _, _, file_count = read_starting_header_part(archive)
 
         if not check_signature(signature):
             return
