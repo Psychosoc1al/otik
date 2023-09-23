ec3ba49840340d83546c56dec4c3eef2f8b23800
Anton Petrov <tohichpetrov@gmail.com>
2023-09-23 15:51:46 +0300

    Add encoded file length field

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from encode import encode
 if __name__ == '__main__':
     action = input('Encode or Decode? (e/d): ')
 
-    if action == 'e':
+    if action in 'eу':
         files_paths = set(input('Enter file [full or relative] paths separated by vertical bar (|): ').split('|'))
         archive_path = input('Enter archive [full or relative] path: ')
         encode(files_paths, archive_path)
-    elif action == 'd':
+    elif action in 'dв':
         archive_path = input('Enter archive [full or relative] path: ')
         output_folder = input('Enter output folder [full or relative] path: ')
         decode(archive_path, output_folder)
