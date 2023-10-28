92b178e53ebbdb63d5c2a011450e419493424bd4
ZzVaMPIRzZ <mitroshin.dmitry2003@gmail.com>
2023-10-28 22:19:13 +0300

    add comments

--- Haf.py
+++ Haf.py
@@ -15,10 +15,12 @@ class Tree:
     def __lt__(self, other: 'Tree'):
         return self._index < other.index
 
+    # Создал для того, чтобы была нумерация у узлов дерева для сортировки
     @property
     def index(self):
         return self._index
 
+    # Левая ветка, 0
     @property
     def left(self):
         return self._left
@@ -27,6 +29,7 @@ class Tree:
     def left(self, value):
         self._left = value
 
+    # Правая ветка, 1
     @property
     def right(self):
         return self._right
@@ -35,9 +38,11 @@ class Tree:
     def right(self, value):
         self._right = value
 
-    def get_table(self):
+    # Создаёт словарь, где ключ - символ, значение - его код
+    def get_table(self) -> dict:
         return dict(self.__get_array(''))
 
+    # Используется в методе выше
     def __get_array(self, code):
         if type(self._left) != Tree and type(self._right) != Tree:
             return (self._left, code + '0'), (self._right, code + '1')
@@ -48,6 +53,8 @@ class Tree:
         else:
             return *(self._left.__get_array(code + '0')), *(self._right.__get_array(code + '1'))
 
+    # Генерирует дерево по списку пар (символ или узел дерева, количество)
+    # Возвращает корень дерева
     @staticmethod
     def generate_tree(array):
         arr = list(filter(lambda x: x[1] != 0, array))
@@ -61,6 +68,9 @@ class Tree:
         return arr[0][0]
 
 
+# Кодирует сообщение по алгоритму Хаффмана
+# На входе - сообщение в символах
+# На выходе - сообщение в бинарном виде
 def encode_haf_mes(message):
     print(message)
     counter = Counter(message)
@@ -73,6 +83,9 @@ def encode_haf_mes(message):
     return message1
 
 
+# Декодирует сообщение по алгоритму Хаффмана
+# На входе - сообщение в бинарном виде и таблица частот
+# На выходе - сообщение в символах
 def decode_haf_mes(message1, table):
     message2 = ''
     tree = Tree.generate_tree(table)
@@ -87,3 +100,9 @@ def decode_haf_mes(message1, table):
             t = tree
 
     return message2
+
+
+# TODO:
+# продумать формат архива
+# приспособить к работе с файлами
+# подключить к основной программе
